85a61ea00200987de50d6abfd45b532e1530d08d
tuanmcwar <tuanntch1@gmail.com>
2022-06-19 16:25:28 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,319 @@
+// var button = document.querySelectorAll(".btn-active");
+// for (var i = 0; i < button.length; i++) {
+//   let index = i;
+//   if (i == 0) {
+//     button[i].classList.add("active");
+//   }
+//   button[i].addEventListener("click", function () {
+//     for (var j = 0; j < button.length; j++) {
+//       if (index != j) {
+//         button[j].classList.remove("active");
+//       } else {
+//         button[j].classList.add("active");
+//       }
+//     }
+//   });
+// }
+document.getElementById("bai1").focus();
+
+document.getElementById("bai1").onclick = function () {
+  var Bai1 = document.getElementById("content1");
+  Bai1.style.display = "block";
+
+  // var Bai1 = document.getElementById("bai1");
+  // Bai1.innerHTML = "📖Bài 1";
+
+  var Bai1 = document.getElementById("content2");
+  Bai1.style.display = "none";
+  var Bai1 = document.getElementById("content3");
+  Bai1.style.display = "none";
+  var Bai1 = document.getElementById("content4");
+  Bai1.style.display = "none";
+  var Bai1 = document.getElementById("content5");
+  Bai1.style.display = "none";
+  var Bai1 = document.getElementById("content6");
+  Bai1.style.display = "none";
+  var Bai1 = document.getElementById("content7");
+  Bai1.style.display = "none";
+};
+// function Bai1() {
+//     var Bai1 = document.getElementById("content1");
+//     Bai1.style.display = "block";
+// }
+document.getElementById("bai2").onclick = function () {
+  var Bai2 = document.getElementById("content2");
+  Bai2.style.display = "block";
+  // var Bai2 = document.getElementById("bai2");
+  // Bai2.innerHTML = "📖Bài 2";
+
+  var Bai2 = document.getElementById("content1");
+  Bai2.style.display = "none";
+  var Bai2 = document.getElementById("content3");
+  Bai2.style.display = "none";
+  var Bai2 = document.getElementById("content4");
+  Bai2.style.display = "none";
+  var Bai2 = document.getElementById("content5");
+  Bai2.style.display = "none";
+  var Bai2 = document.getElementById("content6");
+  Bai2.style.display = "none";
+  var Bai2 = document.getElementById("content7");
+  Bai2.style.display = "none";
+};
+document.getElementById("bai3").onclick = function () {
+  var Bai3 = document.getElementById("content3");
+  Bai3.style.display = "block";
+  // var Bai3 = document.getElementById("bai3");
+  // Bai3.innerHTML = "📖Bài 3";
+  var Bai3 = document.getElementById("content1");
+  Bai3.style.display = "none";
+  var Bai3 = document.getElementById("content2");
+  Bai3.style.display = "none";
+  var Bai3 = document.getElementById("content4");
+  Bai3.style.display = "none";
+  var Bai3 = document.getElementById("content5");
+  Bai3.style.display = "none";
+  var Bai3 = document.getElementById("content6");
+  Bai3.style.display = "none";
+  var Bai3 = document.getElementById("content7");
+  Bai3.style.display = "none";
+};
+document.getElementById("bai4").onclick = function () {
+  var Bai4 = document.getElementById("content4");
+  Bai4.style.display = "block";
+  // var Bai4 = document.getElementById("bai4");
+  // Bai4.innerHTML = "📖Bài 4";
+  var Bai4 = document.getElementById("content1");
+  Bai4.style.display = "none";
+  var Bai4 = document.getElementById("content2");
+  Bai4.style.display = "none";
+  var Bai4 = document.getElementById("content3");
+  Bai4.style.display = "none";
+  var Bai4 = document.getElementById("content5");
+  Bai4.style.display = "none";
+  var Bai4 = document.getElementById("content6");
+  Bai4.style.display = "none";
+  var Bai4 = document.getElementById("content7");
+  Bai4.style.display = "none";
+  var Bai4 = document.getElementById("content8");
+  Bai4.style.display = "none";
+};
+document.getElementById("bai5").onclick = function () {
+  var Bai5 = document.getElementById("content5");
+  Bai5.style.display = "block";
+  // var Bai5 = document.getElementById("bai5");
+  // Bai5.innerHTML = "📖Bài 5";
+  var Bai5 = document.getElementById("content1");
+  Bai5.style.display = "none";
+  var Bai5 = document.getElementById("content2");
+  Bai5.style.display = "none";
+  var Bai5 = document.getElementById("content3");
+  Bai5.style.display = "none";
+  var Bai5 = document.getElementById("content4");
+  Bai5.style.display = "none";
+  var Bai5 = document.getElementById("content6");
+  Bai5.style.display = "none";
+  var Bai5 = document.getElementById("content7");
+  Bai5.style.display = "none";
+  var Bai5 = document.getElementById("content7");
+  Bai5.style.display = "none";
+};
+// // bài 6
+document.getElementById("bai6").onclick = function () {
+  var Bai6 = document.getElementById("content6");
+  Bai6.style.display = "block";
+  // var Bai5 = document.getElementById("bai5");
+  // Bai5.innerHTML = "📖Bài 5";
+  var Bai6 = document.getElementById("content1");
+  Bai6.style.display = "none";
+  var Bai6 = document.getElementById("content2");
+  Bai6.style.display = "none";
+  var Bai6 = document.getElementById("content3");
+  Bai6.style.display = "none";
+  var Bai6 = document.getElementById("content4");
+  Bai6.style.display = "none";
+  var Bai6 = document.getElementById("content5");
+  Bai6.style.display = "none";
+  var Bai6 = document.getElementById("content7");
+  Bai6.style.display = "none";
+  var Bai6 = document.getElementById("content8");
+  Bai6.style.display = "none";
+};
+// // bài 7
+document.getElementById("bai7").onclick = function () {
+  var Bai7 = document.getElementById("content7");
+  Bai7.style.display = "block";
+  // var Bai5 = document.getElementById("bai5");
+  // Bai5.innerHTML = "📖Bài 5";
+  var Bai7 = document.getElementById("content1");
+  Bai7.style.display = "none";
+  var Bai7 = document.getElementById("content2");
+  Bai7.style.display = "none";
+  var Bai7 = document.getElementById("content3");
+  Bai7.style.display = "none";
+  var Bai7 = document.getElementById("content4");
+  Bai7.style.display = "none";
+  var Bai7 = document.getElementById("content5");
+  Bai7.style.display = "none";
+  var Bai7 = document.getElementById("content6");
+  Bai7.style.display = "none";
+  var Bai7 = document.getElementById("content8");
+  Bai7.style.display = "none";
+};
+// // bài 8
+// document.getElementById("bai8").onclick = function () {
+//   var Bai8 = document.getElementById("content8");
+//   Bai8.style.display = "block";
+//   // var Bai5 = document.getElementById("bai5");
+//   // Bai5.innerHTML = "📖Bài 5";
+//   var Bai8 = document.getElementById("content1");
+//   Bai8.style.display = "none";
+//   var Bai8 = document.getElementById("content2");
+//   Bai8.style.display = "none";
+//   var Bai8 = document.getElementById("content3");
+//   Bai8.style.display = "none";
+//   var Bai8 = document.getElementById("content4");
+//   Bai8.style.display = "none";
+//   var Bai8 = document.getElementById("content5");
+//   Bai8.style.display = "none";
+//   var Bai8 = document.getElementById("content6");
+//   Bai8.style.display = "none";
+//   var Bai8 = document.getElementById("content7");
+//   Bai8.style.display = "none";
+// };
+//  bài 1
+function Xuatsochiahetcho3() {
+  var so1 = 100;
+  var n1 = 0;
+  var chuoi1 = "";
+  var chuoi2 = "";
+
+  while (n1 < so1) {
+    if (n1 % 2 === 0) {
+      chuoi1 += n1 + " ";
+    } else {
+      chuoi2 += n1 + " ";
+    }
+    n1++;
+  }
+  document.getElementById("txtB1").innerHTML =
+    "Số chẳn: " + chuoi1 + "</br>" + " 👉Số lẻ: " + chuoi2;
+}
+document.getElementById("btnB1").onclick = function () {
+  Xuatsochiahetcho3();
+};
+// // bài 2
+function Chiahetcho3() {
+  var n = 0;
+  var count = 0;
+
+  while (n <= 1000) {
+    if (n % 3 === 0) {
+      count++;
+    }
+    n++;
+  }
+  document.getElementById("txtB2").innerHTML =
+    "Số chia hết cho 3 nhỏ hơn 1000: " + count + " số ";
+}
+document.getElementById("btnB2").onclick = function () {
+  Chiahetcho3();
+};
+// Bài 3
+function DemsoND() {
+  var sum = 0;
+  var i = 1;
+  var count = 0;
+  while (i < 10000) {
+    if (sum < 10000) {
+      count++;
+      sum += i;
+    }
+    i++;
+  }
+  document.getElementById("txtB3").innerHTML =
+    "Số nguyên dương nhỏ nhất: " + count;
+}
+document.getElementById("btnB3").onclick = function () {
+  DemsoND();
+};
+// bài 4
+function TinhTong() {
+  var x4 = Number(document.getElementById("num1B4").value);
+  var n4 = Number(document.getElementById("num2B4").value);
+  var TongB4 = 0;
+  var Luythua = 1;
+
+  while (Luythua <= n4) {
+    TongB4 += Math.pow(x4, Luythua);
+    Luythua++;
+  }
+  document.getElementById("txtB4").innerHTML = "Tổng: " + TongB4;
+}
+document.getElementById("btnB4").onclick = function () {
+  TinhTong();
+};
+// bài 5
+function GiaiThua() {
+  var sob5 = Number(document.getElementById("num1B5").value);
+  var Giaithua = 1;
+  var sohang = 1;
+  while (sohang <= sob5) {
+    Giaithua = Giaithua * sohang;
+    sohang++;
+  }
+  document.getElementById("txtB5").innerHTML = "Giai thừa: " + Giaithua;
+}
+document.getElementById("btnB5").onclick = function () {
+  GiaiThua();
+};
+// bài 6
+function inDiv() {
+  var soThe = 10;
+  var soDem = 1;
+  var html = "";
+  while (soDem <= soThe) {
+    if (soDem % 2 === 0) {
+      var IntheDIv =
+        "<div style='background: red;line-height:40px;padding-left: 20px;color: #fff;'> Div chản </div>";
+
+      html += IntheDIv;
+    } else {
+      var IntheDIv =
+        "<div style='background: blue;line-height:40px;padding-left: 20px;color: #fff; '> Div lẻ </div>";
+      html += IntheDIv;
+    }
+    soDem++;
+  }
+  document.getElementById("txtB6").innerHTML = html;
+}
+document.getElementById("btnB6").onclick = function () {
+  inDiv();
+};
+
+function isNt(n) {
+  var i = 2;
+  while (i <= Math.sqrt(n)) {
+    if (n % i == 0) {
+      return false;
+    }
+    i++;
+  }
+  return true;
+}
+
+function DemSNT() {
+  var soNT = Number(document.getElementById("num1B7").value);
+  var string = "";
+  var n = 2;
+  while (n <= soNT) {
+    if (isNt(n)) {
+      string += n + " ";
+    }
+    n++;
+  }
+  document.getElementById("txtB7").innerHTML = string;
+}
+
+document.getElementById("btnB7").onclick = function () {
+  DemSNT();
+};
